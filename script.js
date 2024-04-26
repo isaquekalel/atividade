7de47658fe3cf3779837b1f55ea11e3743a06ac0
isaquekalel <isaquekalel@hotmail.com>
2024-04-26 17:12:14 -0500

    fist commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var idade = prompt("insira sua idade")
+var b 
+
+if (idade >= 18) {
+    document.write("Acesso concedido!")
+} else {
+    alert("Desculpe, você não pode acessar a festa.")
+}
+
+if (b = "sim") {
+    document.write("Acesso concedido!")
+} else (b = "não") {
+    alert("Desculpe, você não pode acessar a festa.")
+}
